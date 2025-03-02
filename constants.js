a573686f1e1f67519135a5d5b67fba4f4c51fb0a
Albert Lahat <lahat.albert@gmail.com>
2025-03-01 19:43:36 -0500

    Added Poland

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 // Global variables
 let userGender = '';
 let currentLevel = 0;
-const totalLevels = 20; // Levels: 0, 1, 2
+const totalLevels = 21; // Levels: 0, 1, 2
 let userIP = "";
 let userCountry = "";
 let correctAnswers = {};  // Majority answer per question per gender
